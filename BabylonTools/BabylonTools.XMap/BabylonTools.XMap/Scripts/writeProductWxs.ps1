b86f85dd4f8fe4476f64e90beb643b7f6e3b0616
Arezki Saba <saba.arezki@outlook.com>
2020-10-19 23:18:04 +0200

    ...

---
+++ BabylonTools/BabylonTools.XMap/BabylonTools.XMap/Scripts/writeProductWxs.ps1
@@ -0,0 +1,18 @@
+﻿param(
+	[string] $path
+)
+
+$items = Get-ChildItem $path
+$xml = ""
+
+foreach ($item in $items) {
+	if (!$item.PSIsContainer) {
+		$relativePath = $item.FullName.Replace("$path", "`$`(var.BabylonTools.XMap.TargetDir`)")
+		#$xml += "<File Source=`"$relativePath`"/>`n"
+		$guid = (New-Guid).Guid;
+		$otherGuid = $guid.Replace("-", "")
+		Write-Host "<Component Id=`"$($item.Name).$otherGuid`" Guid=`"$guid`"><File Id=`"$($item.Name).$otherGuid`" Name=`"$($item.Name)`" Source=`"$relativePath`"/></Component>"
+	}
+	
+	#Write-Host $xml
+}
